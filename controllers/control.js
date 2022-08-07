7b42e1d98c2899ac171c95bc0727a6447b0418a8
Ross Morsali <ross@weareelevate.com>
2022-08-07 20:31:20 +0200

    Move controllers into their own files

--- controllers/control.js
+++ controllers/control.js
@@ -1,1 +1,228 @@
 
+const {
+	getExpressionArgs,
+} = require( '../utils' );
+const { getLanguageListCallExpression } = require( './list' );
+class ControlController {
+	constructor( vars, contextName, babel ) {
+		this.vars = vars;
+		this.babel = babel;
+		this.contextName = contextName;
+		this.updateTernaryConditions = this.updateTernaryConditions.bind( this );
+		this.updateTernaryExpressions = this.updateTernaryExpressions.bind( this );
+		this.getExpressionStatement = this.getExpressionStatement.bind( this );
+		this.updateJSXExpressions = this.updateJSXExpressions.bind( this );
+	}
+	updateTernaryConditions( path ) {
+		const { types } = this.babel;
+		// We want tp update the ternary control vars before replace vars (so we can use them at the same time);
+		// Use the identifier visitor to find any identifiers in ternary expressions.
+		if ( this.vars.names.includes( path.node.name ) ) {
+			const excludeTypes = [ 'ObjectProperty', 'ArrayPattern' ];
+			if ( path.parentPath.node && ! excludeTypes.includes( path.parentPath.node.type ) ) {
+
+				const parentNode = path.parentPath.node;
+				const parentParentNode = path.parentPath.parentPath.node;
+
+				// Supports:
+				// const x = test === 'yes' ? 'a' : 'b';
+				// let x; x = test ? 'a' : 'b';
+				// const x = 'prefix-' + ( test ? 'a' : 'b' ) + '-suffix';
+				// let x; x = 'prefix-' + ( test === false ? 'a' : 'b' ) + '-suffix';
+				// And more, only looks for a ternary expression to match.
+				// Should match anything that looks like: `( test ? 'a' : 'b' )`
+				let ternaryExpression;
+				let ternaryExpressionPath;
+				// We need to check if parenNode is a ternary expression.
+				if ( isTernaryExpression( parentNode, types ) ) {
+					ternaryExpression = parentNode;
+					ternaryExpressionPath = path.parentPath;
+				} else if ( isTernaryExpression( parentParentNode, types ) ) {
+					ternaryExpression = parentParentNode;
+					ternaryExpressionPath = path.parentPath.parentPath;
+				}
+				if ( ternaryExpression && ternaryExpressionPath ) {
+					this.updateTernaryExpressions( ternaryExpression, ternaryExpressionPath );
+				}
+				
+			}
+		}
+	}
+	updateTernaryExpressions( expressionSource, currentPath ) {
+		const { types } = this.babel;
+		if ( ! ( expressionSource.test && expressionSource.consequent && expressionSource.alternate ) ) {
+			return;
+		}
+
+		const { statementType, args } = this.getExpressionStatement( expressionSource.test );
+		
+		if ( statementType && args.length > 0 ) {
+			// Build the opening and closing expression tags.
+			const { types } = this.babel;
+			const controlStartString = getLanguageControlCallExpression( [ statementType, 'open' ], args, this.contextName, types );
+			const controlStopString = getLanguageControlCallExpression( [ statementType, 'close' ], args, this.contextName, types );
+			const controlElseStartString = getLanguageControlCallExpression( [ 'else', 'open' ], args, this.contextName, types );
+			const controlElseStopString = getLanguageControlCallExpression( [ 'else', 'close' ], args, this.contextName, types );
+			
+			// create a new expression to add together 3 strings
+			// if = expressionSource.consequent
+			// else = expressionSource.alternate
+			// Create a binary expression with the + operator
+			const parts = [
+				controlStartString,
+				expressionSource.consequent,
+				controlStopString,
+				controlElseStartString,
+				expressionSource.alternate,
+				controlElseStopString,
+			];
+			const combinedBinaryExpression = createCombinedBinaryExpression( parts, '+', types );
+			currentPath.replaceWith( combinedBinaryExpression );
+		}
+	}
+
+	getExpressionStatement( sourceExpression ) {
+
+		const { types } = this.babel;
+
+		let statementType;
+
+		const args = getExpressionArgs( sourceExpression, types );
+		
+		let conditionsMatched = 0;
+		let identifierCount = 0;
+		args.forEach( arg => {
+			if ( arg.type === 'identifier' ) {
+				identifierCount++;
+				if ( this.vars.names.includes( arg.value ) ) {
+					conditionsMatched++;
+				}
+			}
+		} );
+
+		// Return if there are no identifiers or conditions matched.
+		if ( identifierCount === 0 || conditionsMatched === 0 ) {
+			return { args, statementType };
+		}
+
+		// map these to handlebars helper functions and replace the expression with the helper tag.
+		if ( sourceExpression.type === 'Identifier' ) {
+			statementType = 'ifTruthy';
+		} else if ( sourceExpression.type === 'UnaryExpression' ) {
+			if ( sourceExpression.operator === '!' ) {
+				statementType = 'ifFalsy';
+			}
+		} else if( sourceExpression.type === 'BinaryExpression' ) {
+			if ( sourceExpression.operator === '===' ) {
+				statementType = 'ifEqual';
+			} else if ( sourceExpression.operator === '!==' ) {
+				statementType = 'ifNotEqual';
+			}
+		}
+
+		return {
+			args,
+			statementType,
+		}
+
+	}
+
+	updateJSXExpressions( expressionSource, currentPath, listVarsToTag ) {
+
+		if ( ! isControlExpression( expressionSource ) ) {
+			return;
+		}
+
+		const { statementType, args } = this.getExpressionStatement( expressionSource.left );
+
+		if ( statementType && args.length > 0 ) {
+			const { types } = this.babel;
+			const controlStartString = getLanguageControlCallExpression( [ statementType, 'open' ], args, this.contextName, types );
+			const controlStopString = getLanguageControlCallExpression( [ statementType, 'close' ], args, this.contextName, types );
+			currentPath.insertBefore( controlStartString );
+			currentPath.insertAfter( controlStopString );
+
+			// Now check to see if the right of the expression is a list variable, as we need to wrap them
+			// in helper tags.
+			if ( types.isIdentifier( expressionSource.right ) ) {
+				const objectName = expressionSource.right.name;
+				if ( listVarsToTag[ objectName ] ) {
+					const listVarSourceName = listVarsToTag[ objectName ];
+					const listOpen = getLanguageListCallExpression( 'open', listVarSourceName, this.contextName, types );
+					const listClose = getLanguageListCallExpression( 'close', listVarSourceName, this.contextName, types );
+			
+					currentPath.insertBefore( listOpen );
+					currentPath.insertAfter( listClose );
+				}
+			}
+			
+			// Now replace the whole expression with the right part (remove any conditions to display it)
+			currentPath.replaceWith( expressionSource.right );
+		}
+	}
+	
+};
+
+
+
+function isTernaryExpression( node, types ) {
+	if ( types.isConditionalExpression( node ) ) {
+		if ( node.test && node.consequent && node.alternate ) {
+			return true;
+		}
+	}
+	return false;
+}
+
+
+function createCombinedBinaryExpression( parts, operator, types ) {
+	let expression = parts[ 0 ];
+	for ( let i = 1; i < parts.length; i++ ) {
+		expression = types.binaryExpression( operator, expression, parts[ i ] );
+	}
+	return expression;
+}
+
+
+function getLanguageControlCallExpression( targets, args, context, types ) {
+	const targetsNodes = targets.map( target => types.stringLiteral( target ) );
+
+	// using types, create a new object with the properties "type" and "value":
+	const argsNodes = [];
+	args.map( ( arg ) => {
+		const objectWithProps = types.objectExpression( [
+			types.objectProperty( types.identifier('type'), types.stringLiteral( arg.type ) ),
+			types.objectProperty( types.identifier('value'), types.stringLiteral( arg.value ) ),
+		] );
+		argsNodes.push( objectWithProps );
+
+	} );
+		
+	// const argsNodes = args.map( arg => types.stringLiteral( arg ) );
+	return types.callExpression( types.identifier( 'getLanguageControl' ), [ types.arrayExpression( targetsNodes ), types.arrayExpression( argsNodes ), types.identifier( context ) ] );
+}
+
+/**
+ * Ensures the expression being passed is a supporte control type.
+ *
+ * @param {Object} expression The expression to check
+ * @returns 
+ */
+ function isControlExpression( expression ) {
+	if ( ! expression.left || ! expression.right ) {
+		return false;
+	}
+	const controlExpressionTypes = [
+		'Identifier',
+		'MemberExpression',
+		'UnaryExpression',
+		'LogicalExpression',
+	];
+	if ( controlExpressionTypes.includes( expression.type  ) ) {
+		return true;
+	}
+	return false;
+}
+
+
+module.exports = { ControlController };
--- controllers/list.js
+++ controllers/list.js
@@ -1,1 +1,159 @@
 
+
+class ListController {
+	constructor( vars, contextName, babel ) {
+		this.vars = vars;
+		this.contextName = contextName;
+		this.babel = babel;
+		this.initVars = this.initVars.bind( this );
+		this.buildDeclaration = this.buildDeclaration.bind( this );
+		this.normaliseListVar = this.normaliseListVar.bind( this );
+		this.updateIdentifierNames = this.updateIdentifierNames.bind( this );
+		this.updateJSXListExpressions = this.updateJSXListExpressions.bind( this );
+	}
+	initVars( path ) {
+		// Add the new list vars to to top of the block statement.
+		const { parse, types } = this.babel;
+		const self = this;
+
+		this.vars.raw.forEach( ( templateVar, index ) => {
+			const [ varName, varConfig ] = templateVar;
+			// Alway declare as `let` so we don't need to worry about its usage later.
+			const newAssignmentExpression = self.buildDeclaration( self.vars.mapped[ varName ], varConfig );
+			if ( newAssignmentExpression ) {
+				path.node.body.unshift( newAssignmentExpression );
+			}
+			// Now keep track of the list vars and aliaes we need to tag (and keep track of their original source var)
+			self.vars.toTag[ varName ] = varName;
+			if ( varConfig.aliases ) {
+				varConfig.aliases.forEach( ( alias ) => {
+					self.vars.toTag[ alias ] = varName;
+				} );
+			}
+		} );
+	}
+	// Build the object for the replacement var in list type vars.
+	buildDeclaration( varName, varConfig ) {
+		const { parse, types } = this.babel;
+		const normalisedConfig = this.normaliseListVar( varConfig );
+		const { type, props } = normalisedConfig.child;
+		const self = this;
+		const newProp = [];
+		if ( type === 'object' ) {
+			const childProp = {};
+			const propsArr = [];
+			props.forEach( ( propName ) => {
+				const listObject = getLanguageListCallExpression( 'objectProperty', propName, self.contextName, types );
+				propsArr.push( types.objectProperty( types.identifier( propName ), listObject ) );
+			} );
+			newProp.push( childProp );
+			const templateObject = types.objectExpression( propsArr )
+			const right = types.arrayExpression( [ templateObject ] );
+			
+			const left = types.identifier( varName );
+			return types.variableDeclaration('let', [
+				types.variableDeclarator(left, right),
+			]);
+		} else if ( type === 'primitive' ) {
+			// Then we're dealing with a normal array.
+			// TODO: maybe "primitive" is not the best name for this type.
+			const listPrimitiveString = `let ${ varName } = [ getLanguageList( 'primitive', null, ${ this.contextName } ) ];`;
+			return parse( listPrimitiveString );
+		}
+		return null;
+	}	
+	normaliseListVar( varConfig ) {
+		let normalisedConfig = { 
+			type: 'list',
+			child: { type: 'primitive' }
+		};
+		if ( varConfig ) {
+			normalisedConfig = varConfig;
+			if ( ! varConfig.child ) {
+				normalisedConfig.child = { type: 'primitive' }
+			}
+		}
+		
+		return normalisedConfig;
+	};
+	updateIdentifierNames( path ) {
+		const { types } = this.babel;
+		// We also need to replace any lists / arrays with our own templatevars version.
+		if ( this.vars.names.includes( path.node.name ) ) {
+			const sourceVarName = path.node.name;
+			// Make sure we only replace identifiers that are not props and also that
+			// they are not variable declarations.
+			const excludeTypes = [ 'ObjectProperty', 'VariableDeclarator', 'ArrayPattern' ];
+
+			if ( path.parentPath.node && ! excludeTypes.includes( path.parentPath.node.type ) ) {
+				// We want to only allow one case of a member expression when we find a `const x = y.map(...);`
+				if ( types.isMemberExpression( path.parentPath.node ) ) {
+					// then we want to make sure its a `.map` otherwise we don't want to support it for now.
+					if ( types.isIdentifier( path.parentPath.node.property ) && path.parentPath.node.property.name === 'map' ) {
+						// Inject list context to components inside the map
+						if ( this.vars.mapped[ path.node.name ] ) {
+							path.node.name = this.vars.mapped[ path.node.name ];
+							// If we found a map, we want to track which identifier it was assigned to...
+							if ( types.isCallExpression( path.parentPath.parentPath.node ) && types.isVariableDeclarator( path.parentPath.parentPath.parentPath.node ) ) {
+								// Check if its an identifier and if so, add it to the listVars to tag.
+								if ( types.isIdentifier( path.parentPath.parentPath.parentPath.node.id ) ) {
+									const identifierName = path.parentPath.parentPath.parentPath.node.id.name;
+									this.vars.toTag[ identifierName ] = sourceVarName;
+								}
+							}
+						}
+					} else {
+						// Support other member expressions.
+						path.node.name = this.vars.mapped[ path.node.name ];
+					}
+				} else {
+					path.node.name = this.vars.mapped[ path.node.name ];
+				}
+			}
+		}
+	}
+	updateJSXListExpressions( expressionSource, path ) {
+		const { types } = this.babel;
+		// Now look for identifers only, so we can look for list vars that need tagging.
+		if ( types.isIdentifier( expressionSource ) ) {
+			// Then we should be looking at something like: `{ myVar }`
+			if ( this.vars.toTag[ expressionSource.name ] ) {
+				const listOpen = getLanguageListCallExpression( 'open', this.vars.toTag[ expressionSource.name ], this.contextName, types );
+				const listClose = getLanguageListCallExpression( 'close', this.vars.toTag[ expressionSource.name ], this.contextName, types );
+				path.insertBefore( listOpen );
+				path.insertAfter( listClose );
+			}
+		}
+
+		// Also, lets support list vars that have .map() directly in the JSX (ie, they are not re-assigned to variable before being added to the output)
+		if ( types.isCallExpression( expressionSource ) && types.isMemberExpression( expressionSource.callee ) ) {
+			const memberExpression = expressionSource.callee;
+			if ( types.isIdentifier( memberExpression.property ) && memberExpression.property.name === 'map' ) {
+				// Add the before / after tags to the list.
+				const objectName = memberExpression.object.name;
+
+				if ( this.vars.toTag[ objectName ] ) {
+					// Inject list context to components inside the map
+					// injectContextToJSXElementComponents( subPath, contextIdentifier.name, types );
+				
+					const listVarSourceName = this.vars.toTag[ objectName ];
+					const listOpen = getLanguageListCallExpression( 'open', listVarSourceName, this.contextName, types );
+					const listClose = getLanguageListCallExpression( 'close', listVarSourceName, this.contextName, types );
+					path.insertBefore( listOpen );
+					path.insertAfter( listClose );
+				}
+			}
+		}
+	}
+};
+
+
+function getLanguageListCallExpression( action, name, context, types ) {
+	const nameObject = types.objectExpression( [
+		types.objectProperty( types.identifier('type'), types.stringLiteral( 'identifier' ) ),
+		types.objectProperty( types.identifier('value'), types.stringLiteral( name ) ),
+	] );
+	return types.callExpression( types.identifier( 'getLanguageList' ), [ types.stringLiteral( action ), nameObject, types.identifier( context ) ] );
+}
+
+module.exports = { ListController, getLanguageListCallExpression };
